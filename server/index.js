6e7f9a20e0e1988ffbac9b1f7d01d939f853c54e
22521518 <22521518@gm.uit.edu.vn>
2024-06-06 11:53:58 +0700

    initial backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 
@@ -9,7 +12,8 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    const data = prisma.sanpham.findMany();
+    res.send(data);
 });
 
 app.listen(8080, () => {
