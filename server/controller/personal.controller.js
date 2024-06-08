b9676027f6733953ab531a8d129c70e70063f4c6
225251518 <22521518@gm.uit.edu.vn>
2024-06-08 19:43:55 +0700

    get idenity for staff controller backend

---
+++ server/controller/personal.controller.js
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getIdentity = async (req, res) => {
+    try {
+        const { id } = req.body;
+        if (!id) {
+            return res.status(400).json({ message: "Missing id" });
+        }
+        const acc = await prisma.taikhoan.findUnique({
+            where: {
+                id,
+            },
+        });
+        if (!acc) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+
+        const { email, nhanvienId } = acc;
+        const staff = await prisma.nhanvien.findUnique({
+            where: {
+                id: nhanvienId,
+            },
+        });
+
+        res.json({ ...staff, email });
+    } catch (error) {
+        console.error("Error getting identity", error);
+        res.status(500).json({
+            message: `Error getting identity ${error.message}`,
+        });
+    }
+};
+
+export { getIdentity };
