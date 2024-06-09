3cf83033a487c71e0b2de328938f652403bc59b8
225251518 <22521518@gm.uit.edu.vn>
2024-06-09 13:59:53 +0700

    get info routing in selling app backend

--- server/routes/store_customer.routes.js
+++ server/routes/store_customer.routes.js
@@ -6,10 +6,10 @@ import {
     registerCustomerAccount,
 } from "../controller/auth.controller.js";
 import { getOrderOfCustomer } from "../controller/order.controller.js";
-// import {
-//     getCustomerByEmail,
-//     updateCustomerInfo,
-// } from "../controller/customer.controller.js";
+import {
+    getCustomerByEmail,
+    updateCustomerInfo,
+} from "../controller/customer.controller.js";
 import {
     onAddCart,
     updateCart,
@@ -22,12 +22,12 @@ const router = express.Router();
 router.route("/login").post(loginCustomerAccount);
 router.route("/register").post(registerCustomerAccount);
 router.route("/").post(upsertCustomer);
-// router.route("/:email").get(getCustomerByEmail);
+router.route("/:email").get(getCustomerByEmail);
 router.route("/orders/:id").get(getOrderOfCustomer);
 router.route("/cart/:giohangId").get(getCartDetail);
 router.route("/cart/:giohangId").delete(clearCart);
 router.route("/cart_item/:giohangId").post(removeItem);
 router.route("/cart_add/:giohangId").post(onAddCart);
 router.route("/cart_update/:giohangId").post(updateCart);
-// router.route("/profile/:id").post(updateCustomerInfo);
+router.route("/profile/:id").post(updateCustomerInfo);
 export default router;
