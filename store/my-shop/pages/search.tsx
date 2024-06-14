67a0e0633986ecc88865b3f33d9ba9df5383f02f
225251518 <22521518@gm.uit.edu.vn>
2024-06-14 10:00:57 +0700

    add fetch giohang to search page

--- store/my-shop/pages/search.tsx
+++ store/my-shop/pages/search.tsx
@@ -1,10 +1,53 @@
 import { ProductCard } from "@/components";
 import { baseUrl } from "@/constants/url";
 import { GetServerSidePropsContext, NextPageWithLayout } from "next";
-import React from "react";
+import { getServerSession } from "next-auth";
+import React, { useEffect } from "react";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { useStateContext } from "@/context/StateContext";
 
-const SearchPage: NextPageWithLayout = ({ q, products }: any) => {
-    console.log(products);
+const SearchPage: NextPageWithLayout = ({ q, products, giohang }: any) => {
+    const { setCartItems, setTotalPrice, setTotalQuantities } =
+        useStateContext();
+
+    useEffect(() => {
+        if (giohang) {
+            setCartItems([
+                ...giohang.map(
+                    ({
+                        sanphamId,
+                        soluong,
+                        conlai,
+                        details: { gia, hinhanh, ten },
+                    }: any) => {
+                        return {
+                            id: sanphamId,
+                            soluong: soluong,
+                            gia: gia,
+                            ten: ten,
+                            hinhanh: hinhanh,
+                            conlai: conlai,
+                        };
+                    }
+                ),
+            ]);
+            setTotalQuantities(
+                giohang.reduce(
+                    (acc: number, { soluong }: { soluong: number }) =>
+                        acc + soluong,
+                    0
+                )
+            );
+            setTotalPrice(
+                giohang.reduce(
+                    (acc: number, { soluong, details: { gia } }: any) =>
+                        acc + soluong * gia,
+                    0
+                )
+            );
+        }
+    }, [giohang]);
+
     if (products.length === 0) {
         return (
             <div className="products-heading">
@@ -38,6 +81,19 @@ export default SearchPage;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     const { q } = context.query;
+    const session = await getServerSession(
+        context.req,
+        context.res,
+        authOptions
+    );
+    const sessionData = session ? session.user : null;
+
+    let giohang = [];
+    if (sessionData) {
+        giohang = await fetch(
+            `${baseUrl}/store_customers/cart/${sessionData.giohangId}`
+        ).then((res) => res.json());
+    }
 
     try {
         const res = await fetch(`${baseUrl}/store_products/search?q=${q}`);
@@ -47,12 +103,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         const products = await res.json();
 
         return {
-            props: { q, products },
+            props: { q, products, giohang },
         };
     } catch (error) {
         console.error("Error fetching data:", error);
         return {
-            props: { q, products: [] }, // Return empty products array in case of error
+            props: { q, products: [], giohang }, // Return empty products array in case of error
         };
     }
 }
